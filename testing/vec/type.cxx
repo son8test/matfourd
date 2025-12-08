cc4803513de7bfe99477c82faa781576a4c46987
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-12-08 16:56:57 +0200

    add test vec type

---
+++ testing/vec/type.cxx
@@ -0,0 +1,36 @@
+#include <son8/matfourd/vec/type.hxx>
+#include <son8/matfourd/vec/alias.hxx>
+#include <son8/matfourd/vec/equality.hxx>
+#include <son8/matfourd/print.hxx>
+// std
+#include <iostream>
+
+namespace m4d = son8::matfourd;
+
+
+int main( ) {
+    using Msg = char const *;
+    using Vec2 = m4d::Vec2< int >;
+    using Vec3 = m4d::Vec3< int >;
+    using Vec4 = m4d::Vec4< int >;
+
+    auto failed = 0u;
+    auto test = [&failed]( Msg msg, auto result, auto expect ) {
+        if ( result == expect ) return;
+        ++failed;
+        std::cerr << msg
+            << ", result: " << result
+            << ", expect: " << expect
+            << std::endl;
+    };
+
+    Vec2 vec2{ };
+    Vec3 vec3{ };
+    Vec4 vec4{ };
+
+    test( "Vec2{ }", vec2, Vec2{ 0, 0 } );
+    test( "Vec3{ }", vec3, Vec3{ 0, 0, 0 } );
+    test( "Vec4{ }", vec4, Vec4{ 0, 0, 0, 0 } );
+
+    if ( failed ) return 1;
+}
