4b0abf058285b95953e163c5dfe896786e4692de
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-12-22 04:28:56 +0200

    update test vec type, add x,y,z,w

--- testing/vec/type.cxx
+++ testing/vec/type.cxx
@@ -7,9 +7,8 @@ void son8::main( Args ) {
     auto test = []( Msg msg, auto result, auto expect ) {
         if ( result == expect ) return;
         err << msg
-            << ", result: " << result
-            << ", expect: " << expect
-            << std::endl;
+            << PRINT( result )
+            << PRINTEND( expect );
     };
 
     Vec2 vec2{ };
@@ -23,4 +22,17 @@ void son8::main( Args ) {
     test( "Vec2 size", vec2.size( ), 2u );
     test( "Vec3 size", vec3.size( ), 3u );
     test( "Vec4 size", vec4.size( ), 4u );
+
+    vec2 = { 1, 2 };
+    test( "vec2 x", vec2.x( ), 1 );
+    test( "vec2 y", vec2.y( ), 2 );
+    vec3 = { 1, 2, 4 };
+    test( "vec3 x", vec3.x( ), 1 );
+    test( "vec3 y", vec3.y( ), 2 );
+    test( "vec3 z", vec3.z( ), 4 );
+    vec4 = { 1, 2, 4, 8 };
+    test( "vec4 x", vec4.x( ), 1 );
+    test( "vec4 y", vec4.y( ), 2 );
+    test( "vec4 z", vec4.z( ), 4 );
+    test( "vec4 w", vec4.w( ), 8 );
 }
