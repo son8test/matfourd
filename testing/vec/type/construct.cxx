cfabb770829b178e3e379b1cdbb112aef858eef6
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-12-27 13:26:40 +0200

    add test vec type construct

---
+++ testing/vec/type/construct.cxx
@@ -0,0 +1,41 @@
+#include <testing/vec_alias.hxx>
+#include <son8/matfourd/vec/type.hxx>
+#include <son8/matfourd/vec/equality.hxx>
+#include <son8/matfourd/print.hxx>
+// std
+#include <array>
+
+void son8::main( Args ) {
+    auto test = []( Msg msg, auto result, auto expect ) {
+        if ( result == expect && result == ~expect ) return;
+        err << msg
+            << PRINT( result )
+            << PRINTEND( ~expect );
+    };
+
+    int const x = 1, y = 2;
+
+    using Arr2 = std::array< int, 2 >;
+
+    Col2 vecColCB{ x, y }, vecColRB( x, y );
+    Row2 vecRowCB{ x, y }, vecRowRB( x, y );
+    Arr2 vecArrCB{ x, y }, vecArrRB({x, y});
+
+    Col2 vecUnused [[maybe_unused]]
+        , vec1{ { x, y } }, vec2({{ x, y }})
+        , vec3{ vecColCB }, vec4( vecColRB )
+        , vec5{ vecRowCB }, vec6( vecRowRB )
+        , vec7{ vecArrCB }, vec8( vecArrRB )
+    ;
+
+    Row2 expect{ x, y };
+
+    test( "Vec val curly braces", vec1, expect );
+    test( "Vec val round braces", vec2, expect );
+    test( "Vec col curly braces", vec3, expect );
+    test( "Vec col round braces", vec4, expect );
+    test( "Vec row curly braces", vec5, expect );
+    test( "Vec row round braces", vec6, expect );
+    test( "Vec arr curly braces", vec7, expect );
+    test( "Vec arr round braces", vec8, expect );
+}
