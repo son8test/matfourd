bb5bef50b5208da7c3bc7130decd16a6b67ea079
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-12-28 18:41:57 +0200

    update test move, make Move class variable volatile to avoid optimize away destruct call on flash builds

--- testing/include/testing/move.hxx
+++ testing/include/testing/move.hxx
@@ -6,12 +6,12 @@
 #define TESTING_CREATING 0x1
 
 class Move final {
-    int testing_{ TESTING_CREATING };
+    int volatile testing_{ TESTING_CREATING };
 public:
     using CopyType = Move &;
     using MoveType = Move &&;
     Move( ) = default;
-   ~Move( ) { testing_ = TESTING_DESTRUCT; };
+   ~Move( ) noexcept { testing_ = TESTING_DESTRUCT; };
     operator int( ) noexcept { return testing_; }
     Move( CopyType copy ) noexcept : testing_{ copy.testing_ } { copy.testing_ = TESTING_COPYABLE; }
     Move( MoveType move ) noexcept : testing_{ move.testing_ } { move.testing_ = TESTING_MOVEABLE; }
